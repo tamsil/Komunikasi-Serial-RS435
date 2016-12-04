6c30efd7ad5466ade23e48bf7566e0f2aab6bb58
Tamsil Efendi <tamsilefendi@gmail.com>
2016-12-05 02:39:48 +0700

    Add files via upload

---
+++ RS485_Master.ino
@@ -0,0 +1,16 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(3,2);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Komunikasi RS485 : ");
+  mySerial.begin(9600);
+}
+ 
+void loop() {
+  while(mySerial.available() > 0){
+//    char c = mySerial.read();
+int c = mySerial.read();
+    Serial.write(c);
+  }
+}
---
+++ RS485_Slave.ino
@@ -0,0 +1,14 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(3,2);
+
+
+void setup() {
+  mySerial.begin (9600);
+  Serial.begin(9600);
+}
+ 
+void loop() {
+ int data = analogRead(A0);
+mySerial.println(data);
+  delay(100);
+}
